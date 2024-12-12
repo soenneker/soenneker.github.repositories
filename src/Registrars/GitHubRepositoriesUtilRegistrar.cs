e2e6c9de77af49ea91cf01a4fee25861ac13d1d6
Jake Soenneker <jake@soenneker.com>
2024-12-11 21:13:41 -0600

    building

--- src/Registrars/GitHubRepositoriesUtilRegistrar.cs
+++ src/Registrars/GitHubRepositoriesUtilRegistrar.cs
@@ -28,5 +28,7 @@ public static class GitHubRepositoriesUtilRegistrar
     {
         services.AddGitHubClientUtilAsSingleton();
         services.TryAddScoped<IGitHubRepositoriesUtil, GitHubRepositoriesUtil>();
+
+        return services;
     }
 }
